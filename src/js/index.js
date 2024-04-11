7bfdd018c3a9bb98bdaa3f40c8f47d1846523100
FiilipeRibeiro <Filipecaldas3999@gmail.com>
2024-04-11 19:56:31 -0300

    feat: two seconds for other icon

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,39 @@
 const botoes = document.querySelectorAll(".botao");
-
 const personagens = document.querySelectorAll(".personagem");
+let indiceAtual = 0;
 
-botoes.forEach((botao, indice) => {
-  botao.addEventListener("click", () => {
-    desselecionarBotao();
-    desselecionarPersonagem();
+function selecionarProximoBotao() {
+  desselecionarBotao();
+  desselecionarPersonagem();
 
-    botao.classList.add("selecionado");
+  indiceAtual = (indiceAtual + 1) % botoes.length;
+  botoes[indiceAtual].classList.add("selecionado");
+  personagens[indiceAtual].classList.add("selecionado");
+}
 
-    personagens[indice].classList.add("selecionado");
-  });
-});
+const intervalo = setInterval(selecionarProximoBotao, 2000);
 
 function desselecionarPersonagem() {
-  const personagemSelecionado = document.querySelector(
-    ".personagem.selecionado"
-  );
-  personagemSelecionado.classList.remove("selecionado");
+  const personagemSelecionado = document.querySelector(".personagem.selecionado");
+  if (personagemSelecionado) {
+    personagemSelecionado.classList.remove("selecionado");
+  }
 }
 
 function desselecionarBotao() {
   const botaoSelecionado = document.querySelector(".botao.selecionado");
-  botaoSelecionado.classList.remove("selecionado");
+  if (botaoSelecionado) {
+    botaoSelecionado.classList.remove("selecionado");
+  }
 }
+
+botoes.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    clearInterval(intervalo);
+    desselecionarBotao();
+    desselecionarPersonagem();
+
+    botao.classList.add("selecionado");
+    personagens[indice].classList.add("selecionado");
+  });
+});
